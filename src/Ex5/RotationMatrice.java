f2493b3144e782485ecbee755bbeaca0f9552a4b
Asma <a.laouy2064@uca.ac.ma>
2025-10-22 15:47:31 +0100

    mod

--- src/Ex5/RotationMatrice.java
+++ src/Ex5/RotationMatrice.java
@@ -1,9 +1,50 @@
 package Ex5;
+import java.util.Scanner;
 
 public class RotationMatrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public RotationMatrice() {
-		// TODO Auto-generated constructor stub
-	}
+        int N = sc.nextInt();
+        long[][] A = new long[N][N] ;
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = sc.nextLong(); 
+            }
+        }
+
+        rotate90ClockwiseInPlace(A);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
+    public static void rotate90ClockwiseInPlace(long[][] A) { // ✅ long[][]
+        int N = A.length;
+
+        // Transposition
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                long temp = A[i][j];
+                A[i][j] = A[j][i];
+                A[j][i] = temp;
+            }
+        }
+
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N / 2; j++) {
+                long temp = A[i][j];
+                A[i][j] = A[i][N - 1 - j];
+                A[i][N - 1 - j] = temp;
+            }
+        }
+    }
 }
--- src/ex1/SerieHarmonique.java
+++ src/ex1/SerieHarmonique.java
@@ -2,20 +2,24 @@ package ex1;
 import java.util.Scanner;
 
 public class SerieHarmonique {
-
-	public SerieHarmonique() {
-		Scanner sc = new Scanner(System.in);
+    public SerieHarmonique() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Entrez la valeur de n : ");
         int n = sc.nextInt();
-        
-        double somme = 0.0;
 
+        double somme = 0.0;
         for (int i = 1; i <= n; i++) {
             somme += 1.0 / i;
         }
 
         System.out.println("La somme des " + n + " premiers termes de la série harmonique est : " + somme);
         sc.close();
-	}
+    }
 
+    public static void main(String[] args) {
+        new SerieHarmonique();
+    }
 }
+
+
+
